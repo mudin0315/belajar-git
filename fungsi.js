75f6ed36140ca857268462b6f48bd7b5646391bf
mudin0315 <mudin0315@gmail.com>
2023-07-10 10:10:07 +0700

    Merevisi commit perkalian

--- fungsi.js
+++ fungsi.js
@@ -4,8 +4,8 @@ console.log(add(1, 2));
 const subtrac = (a, b) => a - b;
 console.log(subtrac(5, 2));
 
-const multiply = (a, b) => a * b;
-console.log(multiply(2, 2));
+const multiplyNumber = (a, b) => a * b;
+console.log(multiplyNumber(2, 2));
 
 const divide = (a, b) => a * b;
 console.log(divide(2, 2));
